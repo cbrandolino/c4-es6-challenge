36e333f7d60513fefabdeefba09c0767ffccce4d
Claudio Brandolino <cbrandolino@gmail.com>
2016-01-26 08:49:15 +0100

    Added testing framework

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,27 @@
  var webpack = require('webpack');
 
  module.exports = {
-     entry: './src/main.es6',
-     output: {
-         path: path.resolve(__dirname, 'build'),
-         filename: 'main.js'
-     },
-     module: {
-         loaders: [
-             {
-                 test: /\.js$/,
-                 loader: 'babel-loader',
-                 query: {
-                     presets: ['es2015']
-                 }
-             }
-         ]
-     },
-     stats: {
-         colors: true
-     },
-     devtool: 'source-map'
+    entry: { 
+        main: './src/main.es6',
+        test: './test/main.test.es6'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "[name].js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.es6$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
  };
