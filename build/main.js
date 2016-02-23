4582e9d8e0a8dc396e88d92b7e46f539d4c53e4b
Claudio Brandolino <cbrandolino@gmail.com>
2016-02-23 06:41:43 +0100

    Start crawling upon the shoulders of giants (ugh let's say "pretty tall peeps" considering the fact their api is all over the place) for the Ai.

--- build/main.js
+++ build/main.js
@@ -5874,7 +5874,7 @@
 	
 	var _BoardMarbleSprite2 = _interopRequireDefault(_BoardMarbleSprite);
 	
-	var _Ai = __webpack_require__(340);
+	var _Ai = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"../lib/Ai.es6\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 	
 	var _Ai2 = _interopRequireDefault(_Ai);
 	
@@ -34652,87 +34652,6 @@
 	}(_events.EventEmitter);
 	
 	exports.default = Menu;
-
-/***/ },
-/* 340 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	
-	var _events = __webpack_require__(195);
-	
-	var _BoardModel = __webpack_require__(194);
-	
-	var _BoardModel2 = _interopRequireDefault(_BoardModel);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-	
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-	
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-	
-	var AI = function (_EventEmitter) {
-	  _inherits(AI, _EventEmitter);
-	
-	  function AI(player) {
-	    _classCallCheck(this, AI);
-	
-	    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(AI).call(this));
-	
-	    _this.player = player;
-	    _this.depth = 8;
-	    _this._board = null;
-	    return _this;
-	  }
-	
-	  _createClass(AI, [{
-	    key: 'getMaxMoveScore',
-	    value: function getMaxMoveScore() {
-	      var scores = this.getScores();
-	      var maxScore = Object.keys(scores).reduce(function (a, b) {
-	        return scores[a] > scores[b] ? a : b;
-	      });
-	      this.emit('maxScoreReady', maxScore);
-	      return maxScore;
-	    }
-	  }, {
-	    key: 'cloneBoard',
-	    value: function cloneBoard(board) {
-	      var state = board.state.map(function (a) {
-	        return a.slice();
-	      });
-	      var player = board.currentPlayer;
-	      return new _BoardModel2.default(state, player);
-	    }
-	  }, {
-	    key: 'emptyScoresMap',
-	    value: function emptyScoresMap(board) {
-	      return new Map(board.validColumns.map(function (el) {
-	        return [el, 0];
-	      }));
-	    }
-	  }, {
-	    key: 'board',
-	    get: function get() {
-	      return this._board;
-	    },
-	    set: function set(board) {
-	      this._board = this.cloneBoard(board);
-	    }
-	  }]);
-	
-	  return AI;
-	}(_events.EventEmitter);
-	
-	exports.default = AI;
 
 /***/ }
 /******/ ]);
--- src/lib/Ai.es6
+++ src/lib/Ai.es6
@@ -1,5 +1,6 @@
 import { EventEmitter } from 'events';
 import BoardModel from './BoardModel.es6';
+import AlphaBeta from 'alphabeta';
 
 class AI extends EventEmitter {
 
@@ -8,7 +9,27 @@ class AI extends EventEmitter {
     this.player = player;
     this.depth = 8;
     this._board = null;
+    // YAY finally a use for the full UTF-8 syntax support
+    this.αβ = AlphaBeta(this.αβ);
+    // Like srsly how cool is that? If u squint it looks like APL
   }
+
+  getαβpγℓπ() {
+    // Lol get it? AlphaBetaConfig. Gonna stop now tho.
+  }
+
+  generateMoves(state) {
+    const nextPossibleStates = [];
+ 
+    for (move in a list of possible moves ) { 
+    // use item to create a new state 
+    // push state onto nextPossibleStates 
+  // } 
+ 
+    return nextPossibleStates;
+ 
+  }
+
 
   getMaxMoveScore() {
     const scores = this.getScores();
@@ -26,7 +47,7 @@ class AI extends EventEmitter {
   }
 
   emptyScoresMap(board) {
-    return new Map(board.validColumns.map((el) => [el, 0]));
+    return new Map(board.validColumns.map((el) => el));
   }
 
   get board() {
